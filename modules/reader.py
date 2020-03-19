98945102fa47460f74cdc5dbbe154edfade72d0e
Leo <4.leo.makarov@gmail.com>
2020-03-19 17:07:41 +0300

    [+] добавил путь к папку input [+] добавил подключение workbook

--- modules/reader.py
+++ modules/reader.py
@@ -1,11 +1,15 @@
 from modules.time_manager import converter_time
+from openpyxl import load_workbook
+from modules.consts.common import INPUT_FILE_PATH as input_path
 import os
 
 directory = '../input'
 
 
 def read_row_from_exel(sheet_, row):
-    output_data = converter_time(sheet_.cell(row = row, column = 1).value)              #year, month, day
+    # output_data = converter_time(sheet_.cell(row = row, column = 1).value)              #year, month, day
+    output_data = [2020, 3]
+    output_data.append(sheet_.cell(row = row, column = 1).value)                        #дата
     output_data.append(sheet_.cell(row = row, column = 3).value)                        #name
     output_data.append(sheet_.cell(row = row, column = 4).value)                        #email
     output_data.append(sheet_.cell(row = row, column = 6).value)                        #subject
@@ -15,17 +19,19 @@ def read_row_from_exel(sheet_, row):
 def full_read_exel_file(path):
     book_data = []
     row = 2
-    While True:
+    while True:
+        wb      = load_workbook(path, data_only=True)
+        sheet_  = wb['Worksheet']
         book_data.append(read_row_from_exel(sheet_, row))
         row += 1
         if sheet_.cell(row = row, column = 1).value == None:
             break
     return book_data
 
-def read_all_info(directory):
+def read_all_info():
     all_data = []
-    files = os.listdir(directory)
+    files = os.listdir(input_path)
     exel_files = filter(lambda x: x.endswith('.xlsx'), files)
     for file in exel_files:
-        all_data = [*all_data, *full_read_exel_file(directory+f'/{file}')]
+        all_data = [*all_data, *full_read_exel_file(input_path+f'/{file}')]
     return all_data
