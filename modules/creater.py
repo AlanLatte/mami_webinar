54082bccd7aab2bd772a476c5c263daf9c671043
alanlatte <shukurovfoteh@gmail.com>
2020-03-19 14:40:47 +0300

    [+] creater update

--- modules/creater.py
+++ modules/creater.py
@@ -1,38 +1,36 @@
 # приходит dict
 # вовзарщаю name, user_id, year, month, day, hour_s, minut_s
 
-def create_event(name, user_id, year, month, day, hour_s, minut_s):
+def create_event(params: dict):
     url     =   'https://userapi.webinar.ru/v3/events'
     body    =   {
-                    'name'                  :   str(name),
+                    'name'                  :   str(params['name']),
                     'access'                :   '1',
-                    'startsAt[date][year]'  :   str(year),
-                    'startsAt[date][month]' :   str(month),
-                    'startsAt[date][day]'   :   str(day),
-                    'startsAt[time][hour]'  :   str(hour_s),
-                    'startsAt[time][minute]':   str(minut_s),
-                    'lectorids'             :   str(user_id),
-                    'ownerId'               :   str(user_id),
+                    'startsAt[date][year]'  :   str(params['user_id']),
+                    'startsAt[date][month]' :   str(params['year']),
+                    'startsAt[date][day]'   :   str(params['day']),
+                    'startsAt[time][hour]'  :   str(params['hour_s']),
+                    'startsAt[time][minute]':   str(params['minut_s']),
+                    'lectorids'             :   str(params['user_id']),
+                    'ownerId'               :   str(params['user_id']),
                     'type'                  :   'webinar',
                     'duration'              :   'PT1H30M0S',
                 }
     answer = requests.post(url, data=body, headers=headers)
-    print(answer.json())
     return answer.json()['eventId']
 
-def create_event_session(event_id, name, year, month, day, hour_s, minute_s):
+def create_event_session(params: dict):
     url     =   f'https://userapi.webinar.ru/v3/events/{str(event_id)}/sessions'
     body    =   {
-                    'name'                  :   str(name),
+                    'name'                  :   str(params['name']),
                     'access'                :   '1',
                     'lang'                  :   'RU',
-                    'startsAt[date][year]'  :   str(year),
-                    'startsAt[date][month]' :   str(month),
-                    'startsAt[date][day]'   :   str(day),
-                    'startsAt[time][hour]'  :   str(hour_s),
-                    'startsAt[time][minute]':   str(minute_s),
+                    'startsAt[date][year]'  :   str(params['year']),
+                    'startsAt[date][month]' :   str(params['month']),
+                    'startsAt[date][day]'   :   str(params['day']),
+                    'startsAt[time][hour]'  :   str(params['hour_s']),
+                    'startsAt[time][minute]':   str(params['minute_s']),
                 }
 
     answer = requests.post(url, data=body, headers=headers).json()
-    print(answer)
     return answer['eventSessionId'], answer['link']
