b2318fb18c735cc44e5b5f3a5bd583aa53418570
Leo <4.leo.makarov@gmail.com>
2020-03-19 17:28:41 +0300

    [-] удалил неиспользуемый код

--- main.py
+++ main.py
@@ -18,42 +18,6 @@ def change_path(file: str) -> None:
         )
     )
 
-def create_event(name, user_id, year, month, day, hour_s, minut_s):
-    url     =   'https://userapi.webinar.ru/v3/events'
-    body    =   {
-                    'name'                  :   str(name),
-                    'access'                :   '1',
-                    'startsAt[date][year]'  :   str(year),
-                    'startsAt[date][month]' :   str(month),
-                    'startsAt[date][day]'   :   str(day),
-                    'startsAt[time][hour]'  :   str(hour_s),
-                    'startsAt[time][minute]':   str(minut_s),
-                    'lectorids'             :   str(user_id),
-                    'ownerId'               :   str(user_id),
-                    'type'                  :   'webinar',
-                    'duration'              :   'PT1H30M0S',
-                }
-    answer = requests.post(url, data=body, headers=headers)
-    print(answer.json())
-    return answer.json()['eventId']
-
-def create_event_session(event_id, name, year, month, day, hour_s, minute_s):
-    url     =   f'https://userapi.webinar.ru/v3/events/{str(event_id)}/sessions'
-    body    =   {
-                    'name'                  :   str(name),
-                    'access'                :   '1',
-                    'lang'                  :   'RU',
-                    'startsAt[date][year]'  :   str(year),
-                    'startsAt[date][month]' :   str(month),
-                    'startsAt[date][day]'   :   str(day),
-                    'startsAt[time][hour]'  :   str(hour_s),
-                    'startsAt[time][minute]':   str(minute_s),
-                }
-
-    answer = requests.post(url, data=body, headers=headers).json()
-    print(answer)
-    return answer['eventSessionId'], answer['link']
-
 def main():
     id = 0
     info = read_all_info()
@@ -68,5 +32,5 @@ def main():
 if __name__ == '__main__':
     # file = 'main__03.xlsx'
     # change_path(file)
-    # main()
+    main()
     pass
