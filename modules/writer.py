83439385c69c555f71a0eb128d1ba5cf501c6d3c
alanlatte <shukurovfoteh@gmail.com>
2020-03-19 16:45:58 +0300

    [+] создал create_workbook

--- modules/writer.py
+++ modules/writer.py
@@ -1,13 +1,26 @@
-import openpyxl
+import os
+from openpyxl import load_workbook
+from openpyxl import Workbook
+OUTPUT_DIR_PATH = "../output"
 
-# def write_data(sheet_, wb, event_session_id, link, room, row, path):
-#     sheet_.cell(row = row, column = 9).value = link
-#     sheet_.cell(row = row, column = 10).value = room
-#     sheet_.cell(row = row, column = 15).value = event_session_id
-#     wb.save(path)
+def write_data(sheet_, wb, event_session_id, link, room, row, path):
+    sheet_.cell(row = row, column = 9).value = link
+    sheet_.cell(row = row, column = 10).value = room
+    sheet_.cell(row = row, column = 15).value = event_session_id
+    wb.save(path)
 
 
-def write_data(data: lits):
-    for row, object  in enumerate(data):
-        for column, sub_obj in enumerate(object):
-            sheet_.cell(row = row, column=column).value = sub_obj
+def create_workbook(data: list, name: str):
+    OUTPUT_FILE_PATH = f"{OUTPUT_DIR_PATH}/{name}"
+    if name not in os.listdir(OUTPUT_DIR_PATH):
+        work_book = Workbook()
+        work_book.save(filename = str(OUTPUT_FILE_PATH))
+
+    work_book = load_workbook(OUTPUT_FILE_PATH)
+    sheet_names = work_book.sheetnames
+    if sheet_names.__len__() == 1:
+        sheet = work_book[sheet_names[0]]
+        for row, object  in enumerate(data):
+            for column, sub_obj in enumerate(object):
+                sheet.cell(row = int(row)+1, column = int(column)+1).value = sub_obj
+    work_book.save(filename = str(OUTPUT_FILE_PATH))
