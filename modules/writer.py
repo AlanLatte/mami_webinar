f89ce57f0fbe7ab6fde61920ad12daf7e80d3e5f
leo_makarov <4.leo.makarov@gmail.com>
2020-03-20 07:59:04 +0300

    [+] добавил функцию formating_data Функция приводит данные на печать в книгу excel в нужный формат.

--- modules/writer.py
+++ modules/writer.py
@@ -12,7 +12,7 @@ def write_data(sheet, wb, event_session_id, link, room, row, path):
     wb.save(path)
 
 
-def create_workbook(data: dict, name: str, params: dict):
+def create_workbook(data: list, name: str, params: dict):
     if params['type'] == "dev":
         header = (
             "Дата", "ID", "Преподаватель ФИО",
@@ -27,7 +27,7 @@ def create_workbook(data: dict, name: str, params: dict):
     else:
         print("create_workbook need some params!")
         sys.exit()
-    OUTPUT_FILE_PATH = os.path.join(OUTPUT_FILE_PATH, name)
+    OUTPUT_FILE_PATH = os.path.join(OUTPUT_DIR_PATH, name)
 
     data = formating_data(data, params)
 
@@ -50,21 +50,31 @@ def create_workbook(data: dict, name: str, params: dict):
     work_book.save(filename=str(OUTPUT_FILE_PATH))
 
 
-def formating_data(data:dict, params:dict):
+def formating_data(data:list, params:dict):
+    list_data = []
     if params['type'] == "dev":
-        data = list(
-                    data['date'],
-                    data['id'],
-                    data['name'],
-                    data['email'],
-                    data['phone_number'],
-                    data['subject'],
-                    data['start_t'],
-                    data['end_time'],
-                    data['link']
-                    data['room']
-                    data['group'],
-                   )
+        for row in data:
+            list_data.append([
+                               '-'.join(row['date']),
+                               row['id'],
+                               row['name'],
+                               row['email'],
+                               row['phone_number'],
+                               row['subject'],
+                               ':'.join(row['start_t']),
+                               row['end_time'],
+                               row['link'],
+                               row['room'],
+                               row['group']])
     else:
-        data = list(data)
-    return data
+        for row in data:
+            list_data.append([
+                                '-'.join(row['date']),
+                                row['id'],
+                                row['name'],
+                                row['subject'],
+                                ':'.join(row['start_t']),
+                                row['end_time'],
+                                row['link'],
+                             ])
+    return list_data
