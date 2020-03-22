00721104b7b111a32c95bb7d48a48e5585f8fa91
alanlatte <shukurovfoteh@gmail.com>
2020-03-22 18:20:54 +0300

    [+] новый формат вывода в консоль

--- modules/creater.py
+++ modules/creater.py
@@ -4,27 +4,34 @@ import requests
 def create_event(params):
     url = 'https://userapi.webinar.ru/v3/events'
     body = {
-                'name': str(params['subject']),
-                'access': '1',
-                'startsAt[date][year]': str(int(params['date'][0])),
-                'startsAt[date][month]': str(int(params['date'][1])),
-                'startsAt[date][day]': str(int(params['date'][2])),
-                'startsAt[time][hour]': str(int(params['start_t'][0])),
-                'startsAt[time][minute]': str(int(params['start_t'][1])),
-                'lectorids': str(params['user_id']),
-                'ownerId': str(params['user_id']),
-                'type': 'webinar',
-                'duration': 'PT1H30M0S',
-            }
+        'name': str(params['subject']),
+        'access': '1',
+        'startsAt[date][year]': str(int(params['date'][0])),
+        'startsAt[date][month]': str(int(params['date'][1])),
+        'startsAt[date][day]': str(int(params['date'][2])),
+        'startsAt[time][hour]': str(int(params['start_t'][0])),
+        'startsAt[time][minute]': str(int(params['start_t'][1])),
+        'lectorids': str(params['user_id']),
+        'ownerId': str(params['user_id']),
+        'type': 'webinar',
+        'duration': 'PT1H30M0S',
+    }
     answer = requests.post(url, data=body, headers=headers)
     try:
-        print('+======+\n+======+\n')
-        print(f"Создан вебинар \
-    \n\tid: {str(params['id'])}\n\tпредмет: {str(params['subject'])}\
-    \n\t{str(params['start_t'][0])} часов")
+        print(
+            f"""
+    +=============================+
+    Создан вебинар
+        id: {str(params['id'])}
+        предмет: {str(params['subject'])}
+        {str(params['start_t'][0])} часов {str(params['start_t'][1])} минут
+    +=============================+
+            """
+        )
         return answer.json()['eventId']
+
     except KeyError:
-        print('\tВебина не создался')
+        print('\tВебинар не создался')
     except:
         print('\tОшибка в создании вебинара')
 
@@ -32,19 +39,23 @@ def create_event(params):
 def create_event_session(params, event_id):
     url = f'https://userapi.webinar.ru/v3/events/{str(event_id)}/sessions'
     body = {
-                'name': str(params['subject']),
-                'access': '1',
-                'lang': 'RU',
-                'startsAt[date][year]': str(int(params['date'][0])),
-                'startsAt[date][month]': str(int(params['date'][1])),
-                'startsAt[date][day]': str(int(params['date'][2])),
-                'startsAt[time][hour]': str(int(params['start_t'][0])),
-                'startsAt[time][minute]': str(int(params['start_t'][1])),
-            }
+        'name': str(params['subject']),
+        'access': '1',
+        'lang': 'RU',
+        'startsAt[date][year]': str(int(params['date'][0])),
+        'startsAt[date][month]': str(int(params['date'][1])),
+        'startsAt[date][day]': str(int(params['date'][2])),
+        'startsAt[time][hour]': str(int(params['start_t'][0])),
+        'startsAt[time][minute]': str(int(params['start_t'][1])),
+    }
 
     answer = requests.post(url, data=body, headers=headers).json()
     try:
-        print('\tСоздана сессия')
+        print(f"""
+    Создана сессия
+        eventSessionId: {answer['eventSessionId']}
+            """
+        )
         return answer['eventSessionId'], answer['link']
     except KeyError:
         print('\tСессия не создана')
