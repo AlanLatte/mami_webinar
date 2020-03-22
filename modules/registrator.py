9f5f361dc91c448364403cbb973722ec664efbca
leo_makarov <4.leo.makarov@gmail.com>
2020-03-22 13:16:45 +0300

    [+] добавил offline режим в функцию register_to_vebinar

--- modules/registrator.py
+++ modules/registrator.py
@@ -2,7 +2,7 @@ import requests
 from .consts.common import HEADERS as headers
 
 
-def register_to_vebinar(eventsessionID, name, email):
+def register_to_vebinar(eventsessionID, params, type):
 
     """role — роль участника на этом мероприятии. Значения:
 - ADMIN
@@ -11,17 +11,23 @@ def register_to_vebinar(eventsessionID, name, email):
     """
 
     url = f'https://userapi.webinar.ru/v3/eventsessions/{str(eventsessionID)}/register'
-    names = name_refactor(name)
-    emails = email_refactoring(email)
+    names = name_refactor(params['name'])
+    emails = email_refactoring(params['email'])
     for id in range(len(names)):
-        if names[id] == '' or emails[id] == '':
-            continue
-        body = {
-            'name': str(names[id]),
-            'role': 'ADMIN',
-            'email': str(emails[id]),
-        }
-        print(requests.post(url, data=body, headers=headers).json())
+        try:
+            if names[id] == '' or emails[id] == '':
+                continue
+            body = {
+                'name': str(names[id]),
+                'role': 'ADMIN',
+                'email': str(emails[id]),
+            }
+            if type == 'online':
+                print(requests.post(url, data=body, headers=headers).json())
+            else:
+                print(f'Регистрация успешна {names[id]}, email {emails[id]}, предмет {params['subject']}')
+        except:
+            print(f'Произошла ошибка регистрации для пользователя {names[id]}, email {emails[id]}, предмет {params['subject']}')
 
 
 def name_refactor(name):
