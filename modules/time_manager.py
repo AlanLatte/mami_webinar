b2bd9641d9e514680d5c0289662c737fb175af94
alanlatte <shukurovfoteh@gmail.com>
2020-03-23 00:42:52 +0300

    [+] new functions in teme_manager

--- modules/time_manager.py
+++ modules/time_manager.py
@@ -1,3 +1,15 @@
+import datetime
+
 def converter_date(date):
     date_ = str(date).split("-")
     return [date_[0], date_[1], date_[2]]
+
+
+def converter_time(time):
+    return time.split(':')
+
+
+def converter_to_datetime(date_time):
+    return datetime.datetime.strptime(
+        f"{date_time}", "%Y-%m-%d %H:%M"
+    )
