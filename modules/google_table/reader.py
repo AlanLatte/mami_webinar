652b7b8a6aea441faab705dac4f39ed8c08028c0
leo_makarov <4.leo.makarov@gmail.com>
2020-03-23 14:07:36 +0300

    [*] пофиксил баги с циклом [~] вынес count_of_row_in_request в отдельную переменную [~] изменил генерацию имени таблицы

--- modules/google_table/reader.py
+++ modules/google_table/reader.py
@@ -7,7 +7,6 @@ from modules.consts.common import WORKBOOK_HEADER_PRIVATE
 from modules.writer import formating_data
 
 
-
 httpAuth = ServiceAccountCredentials.from_json_keyfile_name(
     CREDENTIALS_FILE,
     [
@@ -18,36 +17,30 @@ httpAuth = ServiceAccountCredentials.from_json_keyfile_name(
 
 service = apiclient.discovery.build('sheets', 'v4', http = httpAuth)
 
-# spreadsheet = service.spreadsheets().get(spreadsheetId = '1e_DqDCwrc3xlTWsXDB-zUuchWSWH_HA6wzogKbXPStE').execute()
-
-
-def read_table(spreadsheetId=SPREAD_SHEET_ID):
+def read_table(spreadsheetId=SPREAD_SHEET_ID, count_of_row_in_request=500):
     spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
     today = datetime.datetime.now().strftime("%Y-%m-%d")
-    # table = f'Расписание на {today}' + '4'
-    table = 'Расписание на 2020-03-234'
+    table = f'Расписание на {today}'
+    # table = f'Расписание на 2020-03-234'
     row = 1
     info = []
     while True:
-        cell_range = f'{table}!A{row}:{chr(64+len(WORKBOOK_HEADER_PRIVATE))}{row+100}'
+        cell_range = f'{table}!A{row}:{chr(64+len(WORKBOOK_HEADER_PRIVATE))}{row+int(count_of_row_in_request)}'
 
         value = service.spreadsheets().values().get(
             spreadsheetId=spreadsheetId,
-            range=cell_range
+            range=cell_range,
+            majorDimension='ROWS'
         ).execute()
 
-        info.append({})
-        for row_id in range(101):
+        for row_in_responce in range(int(count_of_row_in_request)+1):
+            if len(value['values']) <= row_in_responce:
+                break
+            info.append({})
             for i, header in enumerate(WORKBOOK_HEADER_PRIVATE):
-                if value['values'][row_id][i] == '':
-                    break
-                info[-1][header] = value['values'][row_id][i]
+                info[-1][header] = value['values'][row_in_responce][i]
         else:
+            row += int(count_of_row_in_request)
             continue
         break
-
     return info
-
-# def get_counts_of_row(spreadsheetId='1e_DqDCwrc3xlTWsXDB-zUuchWSWH_HA6wzogKbXPStE'):
-#     spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
-#     cell_range = f'{table}!A{row}:{chr(64+len(WORKBOOK_HEADER_PRIVATE))}{row}'
