ce3935e8357e920772a9b2e6be779d13fb3acff2
alanlatte <shukurovfoteh@gmail.com>
2020-03-25 23:13:21 +0300

    [*] bug fixed

--- modules/google_table/checker.py
+++ modules/google_table/checker.py
@@ -1,24 +1,32 @@
 import re
 
-def check_name(sheet_name: str, spreadsheet=None):
+
+def check_name(sheet_name, spreadsheet=None):
     sheets = spreadsheet.get('sheets', '')
     counter_sheets_regexp = re.compile(pattern=r"\((\d)+\)+$")
-    if sheet_name in [
-        sheet['properties']['title'] for sheet in sheets
-    ]:
-        start_position, end_position = [
-            a.span() for a in counter_sheets_regexp.finditer(string=sheet_name)
-        ][0]
-        counter_sheets_number = [
-            a.group()[1:-1] for a in counter_sheets_regexp.finditer(string=sheet_name)
-        ][0]
+    if sheet_name in sheets:
+        counter_information = sum(
+            [
+                [
+                    (
+                        string,
+                        int(finder.group()[1:-1]),
+                        finder.span(),
+                    ) for finder in counter_sheets_regexp.finditer(string)
+                ] for string in [
+                    sheet for sheet in sheets if sheet_name in sheet
+                ] if counter_sheets_regexp.search(string)
+            ],
+            []
+        )
 
-        if counter_sheets_regexp:
-            return sheet_name.replace(
-                sheet_name[
-                    start_position:end_position
+        if counter_information:
+            counter_information = max(counter_information)
+            return counter_information[0].replace(
+                counter_information[0][
+                    counter_information[-1][0]:counter_information[-1][1]
                 ],
-                f"({int(counter_sheets_number)+1})"
+                f"({str(counter_information[1]+1)})"
             )
         else:
             return f"{sheet_name}(1)"
