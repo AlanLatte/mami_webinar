6d2121b6f1d65c9aa85fd026aad427783d6998d2
alanlatte <shukurovfoteh@gmail.com>
2020-03-25 23:12:49 +0300

    [+] settings init

---
+++ modules/settings.py
@@ -0,0 +1,24 @@
+import requests
+from modules.consts.common import HEADERS
+
+
+def chat_options(eventsessionID, params: dict={}):
+    """
+        chat:
+            off
+
+        polls:
+            on
+    """
+    if params['chat'] == 'off':
+        requests.put(
+            url=f"https://userapi.webinar.ru/v3/eventsessions/{eventsessionID}/chat",
+            data={"isModerated" : "off"},
+            headers=HEADERS
+        )
+    if params['polls'] == 'on':
+        requests.put(
+            url=f"https://userapi.webinar.ru/v3/eventsessions/{eventsessionID}/questions/moderate",
+            data={"isModerated" : "true"},
+            headers=HEADERS
+        )
