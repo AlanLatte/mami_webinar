b98917c888ef75bc0b8c4fc5e9375cb555100608
alan <shukurovfoteh@gmail.com>
2020-09-13 17:17:21 +0300

    [+] get rooms

--- additional_utils/user_id.py
+++ additional_utils/user_id.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+from typing import Tuple, List
+
 import requests
 from modules.consts.common import HEADERS
 
@@ -5,3 +8,12 @@ from modules.consts.common import HEADERS
 def get_all_users_id():
     url = "https://userapi.webinar.ru/v3/organization/members"
     return requests.get(url=url, headers=HEADERS).json()
+
+
+def get_rooms() -> List[Tuple[int, str]]:
+    rooms = [
+        (item["id"], f'W{item["secondName"]}')
+        for item in get_all_users_id()
+        if item["name"].lower() == "комната"
+    ]
+    return sorted(rooms, key=lambda x: x[1])
